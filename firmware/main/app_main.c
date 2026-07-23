7c0d7750ee5262975800da880fb57c8b0661e54b
Phillip Chin <ekkus93@gmail.com>
2026-07-23 10:33:33 -0700

    firmware: add ESP-IDF project skeleton

---
+++ firmware/main/app_main.c
@@ -0,0 +1,12 @@
+#include "app_core.h"
+#include "esp_log.h"
+
+static const char *const TAG = "app_main";
+
+void app_main(void)
+{
+    const app_error_code_t result = app_core_start();
+    if (result != APP_ERROR_NONE) {
+        ESP_LOGE(TAG, "startup failed: %s", app_error_code_string(result));
+    }
+}
