bc76d2c585a585b5c87abd6000a72b457365890a
Phillip Chin <ekkus93@gmail.com>
2026-07-23 22:45:20 -0700

    storage: expose injectable parent durability barrier

--- firmware/components/storage/storage_atomic_internal.h
+++ firmware/components/storage/storage_atomic_internal.h
@@ -10,6 +10,18 @@
 
 typedef app_error_code_t (*storage_uuid_generate_fn)(void *context,
                                                      app_uuid_t *out_uuid);
+typedef int (*storage_parent_sync_fn)(void *context, const char *path);
+
+app_error_code_t storage_atomic_write_with_ops_and_parent_sync(
+    const char *path,
+    const void *data,
+    size_t data_length,
+    bool sync_required,
+    const storage_fs_ops_t *operations,
+    storage_uuid_generate_fn generate_uuid,
+    void *uuid_context,
+    storage_parent_sync_fn sync_parent_path,
+    void *parent_sync_context);
 
 app_error_code_t storage_atomic_write_with_ops(const char *path,
                                                const void *data,
