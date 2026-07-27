da722ba91a9d973af7bf33b9bc60b5498011b147
Phillip Chin <ekkus93@gmail.com>
2026-07-26 23:41:49 -0700

    ci(provisioning): gate the production partition table

--- scripts/check-all.sh
+++ scripts/check-all.sh
@@ -8,6 +8,7 @@ cd "${repo_root}"
 ./scripts/verify-toolchain.sh
 ./scripts/check-format.sh
 ./scripts/check-static-analysis-policy.sh
+./scripts/check-partitions.sh
 ./scripts/check-firmware.sh
 ./scripts/check-webapp.sh
 ./scripts/check-scripts.sh
