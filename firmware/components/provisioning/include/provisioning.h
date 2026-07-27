050274c0a46632f22645e6d08c26ba53f2d8ae78
Phillip Chin <ekkus93@gmail.com>
2026-07-27 00:00:40 -0700

    feat(provisioning): define persistent configuration API

---
+++ firmware/components/provisioning/include/provisioning.h
@@ -0,0 +1,37 @@
+#ifndef PROVISIONING_H
+#define PROVISIONING_H
+
+#include <stdbool.h>
+#include <stdint.h>
+
+#include "app_error.h"
+#include "app_uuid.h"
+#include "auth.h"
+#include "wifi_ap.h"
+
+#define PROVISIONING_CREDENTIAL_VERSION_INITIAL 1U
+
+typedef struct {
+    uint32_t schema_version;
+    uint32_t revision;
+    uint32_t credential_version;
+    bool provisioned;
+    char ap_ssid[WIFI_AP_SSID_MAX_BYTES + 1U];
+    char ap_passphrase[WIFI_AP_PASSPHRASE_MAX_BYTES + 1U];
+    auth_password_record_t password_record;
+    bool require_physical_confirmation;
+    bool always_select_set;
+    bool has_active_set;
+    app_uuid_t active_set_id;
+} provisioning_config_t;
+
+app_error_code_t provisioning_init(void);
+app_error_code_t provisioning_load(provisioning_config_t *out_config);
+app_error_code_t provisioning_commit(const provisioning_config_t *replacement,
+                                     uint32_t expected_revision,
+                                     provisioning_config_t *out_committed);
+app_error_code_t provisioning_clear_credentials(void);
+app_error_code_t provisioning_factory_reset(void);
+app_error_code_t provisioning_deinit(void);
+
+#endif
