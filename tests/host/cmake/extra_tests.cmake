e5e4dd5b6a3f74313963a0c0bd11fa7164a8d84b
Phillip Chin <ekkus93@gmail.com>
2026-07-27 09:11:44 -0700

    test(web): register setup policy host target

--- tests/host/cmake/extra_tests.cmake
+++ tests/host/cmake/extra_tests.cmake
@@ -14,3 +14,21 @@ target_link_libraries(provisioning_bootstrap_tests PRIVATE test_support)
 target_compile_options(provisioning_bootstrap_tests PRIVATE ${STRICT_WARNINGS})
 add_test(NAME provisioning_bootstrap COMMAND provisioning_bootstrap_tests)
 set_tests_properties(provisioning_bootstrap PROPERTIES LABELS "storage")
+
+add_executable(
+    web_setup_tests
+    "${CMAKE_SOURCE_DIR}/test_web_setup.c"
+    "${CMAKE_SOURCE_DIR}/../../firmware/components/web_server/web_setup_core.c"
+)
+target_include_directories(
+    web_setup_tests
+    PRIVATE "${CMAKE_SOURCE_DIR}/../../firmware/components/macro_model/include"
+            "${CMAKE_SOURCE_DIR}/../../firmware/components/auth/include"
+            "${CMAKE_SOURCE_DIR}/../../firmware/components/wifi_ap/include"
+            "${CMAKE_SOURCE_DIR}/../../firmware/components/provisioning/include"
+            "${CMAKE_SOURCE_DIR}/../../firmware/components/web_server"
+)
+target_link_libraries(web_setup_tests PRIVATE test_support)
+target_compile_options(web_setup_tests PRIVATE ${STRICT_WARNINGS})
+add_test(NAME web_setup COMMAND web_setup_tests)
+set_tests_properties(web_setup PROPERTIES LABELS "web")
