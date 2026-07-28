48d493b6efb1bc1ba0bfc5d27a885073277ccd36
Phillip Chin <ekkus93@gmail.com>
2026-07-28 03:37:33 -0700

    feat(web): define bounded API response envelopes

---
+++ firmware/components/web_server/web_api_response.h
@@ -0,0 +1,23 @@
+#ifndef WEB_API_RESPONSE_H
+#define WEB_API_RESPONSE_H
+
+#include <stddef.h>
+
+#include "app_error.h"
+
+#define WEB_API_RESPONSE_MAX_BYTES (512U * 1024U)
+
+typedef struct {
+    unsigned int status;
+    char *body;
+    size_t body_length;
+} web_api_response_t;
+
+app_error_code_t web_api_response_success(web_api_response_t *response, unsigned int status,
+                                          const char *data_json);
+app_error_code_t web_api_response_error(web_api_response_t *response, unsigned int status,
+                                        app_error_code_t code, const char *message,
+                                        const char *details_json);
+void web_api_response_free(web_api_response_t *response);
+
+#endif
