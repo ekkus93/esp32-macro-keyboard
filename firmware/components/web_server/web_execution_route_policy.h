9a5b6c58955f1404fc563d3928792b4243397cbf
Phillip Chin <ekkus93@gmail.com>
2026-07-28 09:53:08 -0700

    fix(web): add execution route policy seam

---
+++ firmware/components/web_server/web_execution_route_policy.h
@@ -0,0 +1,22 @@
+#ifndef WEB_EXECUTION_ROUTE_POLICY_H
+#define WEB_EXECUTION_ROUTE_POLICY_H
+
+#include <stdbool.h>
+
+#include "app_error.h"
+#include "macro_executor.h"
+#include "web_api_core.h"
+
+typedef struct {
+    bool permitted;
+    unsigned int status;
+    app_error_code_t error;
+    const char *message;
+} web_execution_cancel_policy_t;
+
+app_error_code_t web_execution_cancel_policy_evaluate(
+    const macro_execution_status_t *execution_status,
+    const web_api_path_t *request_path,
+    web_execution_cancel_policy_t *out_policy);
+
+#endif
