39b264e28e5453c105a703a84d26f5b0faa5bd8f
Phillip Chin <ekkus93@gmail.com>
2026-07-29 15:11:02 -0700

    storage: define bounded package validation API

---
+++ firmware/components/storage/include/storage_package.h
@@ -0,0 +1,27 @@
+#ifndef STORAGE_PACKAGE_H
+#define STORAGE_PACKAGE_H
+
+#include <stddef.h>
+
+#include "app_error.h"
+
+typedef enum {
+    STORAGE_PACKAGE_KIND_SET = 0,
+    STORAGE_PACKAGE_KIND_BACKUP,
+} storage_package_kind_t;
+
+typedef struct {
+    storage_package_kind_t kind;
+    size_t package_bytes;
+    size_t set_count;
+    size_t local_macro_count;
+    size_t global_macro_count;
+    size_t procedure_count;
+    size_t progress_count;
+} storage_package_summary_t;
+
+app_error_code_t storage_package_validate(const char *data, size_t length,
+                                          storage_package_kind_t expected_kind,
+                                          storage_package_summary_t *out_summary);
+
+#endif
