5aa107523692efdea47af2331482b9e19c3ebb30
Phillip Chin <ekkus93@gmail.com>
2026-07-30 23:33:27 -0700

    feat(api): pass administration calls to package boundary

--- firmware/components/web_server/web_api_admin_boundary.h
+++ firmware/components/web_server/web_api_admin_boundary.h
@@ -2,9 +2,10 @@
 #define WEB_API_ADMIN_BOUNDARY_H
 
 #include "app_error.h"
-#include "web_api_core.h"
+#include "web_api_handlers.h"
 #include "web_api_response.h"
 
-app_error_code_t web_api_admin_boundary_handle(web_api_route_t route, web_api_response_t *response);
+app_error_code_t web_api_admin_boundary_handle(const web_api_call_t *call,
+                                               web_api_response_t *response);
 
 #endif
