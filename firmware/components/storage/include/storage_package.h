49854246eda66c2e2741fc088ac8ae21e7154ac1
Phillip Chin <ekkus93@gmail.com>
2026-07-30 18:48:46 -0700

    style(storage): normalize package export declaration

--- firmware/components/storage/include/storage_package.h
+++ firmware/components/storage/include/storage_package.h
@@ -26,7 +26,7 @@ app_error_code_t storage_package_validate(const char *data, size_t length,
                                           storage_package_kind_t expected_kind,
                                           storage_package_summary_t *out_summary);
 app_error_code_t storage_package_export_set(const app_uuid_t *set_id, bool include_progress,
-                                            char **out_data, size_t *out_length);
+                                             char **out_data, size_t *out_length);
 void storage_package_free(char *data);
 
 #endif
