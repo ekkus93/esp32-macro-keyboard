f8885c0ab1961f92a8408c6cdeacb2036c2915ac
Phillip Chin <ekkus93@gmail.com>
2026-08-03 14:52:24 -0700

    Declare canonical v2 named-key lookup

---
+++ firmware/components/macro_parser/include/macro_keymap_us_v2.h
@@ -0,0 +1,10 @@
+#ifndef MACRO_KEYMAP_US_V2_H
+#define MACRO_KEYMAP_US_V2_H
+
+#include <stdbool.h>
+
+#include "macro_keymap_us.h"
+
+bool macro_keymap_us_named_v2_canonical(const char *name, macro_hid_key_t *out_key);
+
+#endif
