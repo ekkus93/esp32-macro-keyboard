0e6b7eab9180a8ae228b4bab49c028243a761990
Phillip Chin <ekkus93@gmail.com>
2026-08-05 17:51:37 -0700

    Declare V2-032 blob handlers

--- firmware/components/web_server/web_server_internal.h
+++ firmware/components/web_server/web_server_internal.h
@@ -38,7 +38,10 @@ esp_err_t status_handler(httpd_req_t *request);
 esp_err_t limits_handler(httpd_req_t *request);
 esp_err_t login_handler(httpd_req_t *request);
 esp_err_t logout_handler(httpd_req_t *request);
+esp_err_t blob_list_handler(httpd_req_t *request);
 esp_err_t blob_create_handler(httpd_req_t *request);
+esp_err_t blob_load_handler(httpd_req_t *request);
+esp_err_t blob_delete_handler(httpd_req_t *request);
 esp_err_t api_handler(httpd_req_t *request);
 esp_err_t web_api_handle_call(httpd_req_t *request, bool *out_should_restart);
 esp_err_t web_api_handle_call_with_body(httpd_req_t *request, char *preread_body,
